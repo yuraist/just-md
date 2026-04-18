c2e4d84c4a46b32aaf50fa8189f2d4df3e5f540d
Yuri <yura.ist@icloud.com>
2026-04-18 12:00:48 -0400

    created a template xcode project for just-md app

---
+++ JustMD/JustMDTests/JustMDTests.swift
@@ -0,0 +1,17 @@
+//
+//  JustMDTests.swift
+//  JustMDTests
+//
+//  Created by Yuri Istomin on 4/18/26.
+//
+
+import Testing
+@testable import JustMD
+
+struct JustMDTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
